ee0d7c353c87e467d2cdafb539448517e07d8906
VLtim43 <fernandoreal.valentim@gmail.com>
2025-03-16 21:11:21 -0300

    read me

--- PA2/run_lexer.sh
+++ PA2/run_lexer.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-# Run gmake clean
 gmake clean
 
 # Build the lexer with custom CFLAGS
 gmake CFLAGS="-g -Wall -Wno-unused -Wno-register -Wno-write-strings -I/var/tmp/cool/include/PA2" lexer
 
-# Run the lexer with input file foo.cl
 ./lexer test.cl
